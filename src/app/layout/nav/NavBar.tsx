8f7e02c2c9a9cee99c6a70e2f2561fd71b2f623e
Kristian <10221425@ucn.dk>
2024-04-28 10:40:58 +0200

    NavBar

---
+++ src/app/layout/nav/NavBar.tsx
@@ -0,0 +1,22 @@
+import { Button, Container, Menu, MenuItem } from "semantic-ui-react"
+
+export default function NavBar() {
+  return (
+    <Menu inverted fixed="top">
+      <Container>
+        <MenuItem header>
+          <img src="logo.png" alt="logo" style={{ marginRight: 15 }} />
+          Re-vents
+        </MenuItem>
+        <MenuItem name="Events" />
+        <MenuItem>
+          <Button floated="right" positive inverted content="Create Event" />
+        </MenuItem>
+        <MenuItem position="right">
+          <Button basic inverted content="Login" />
+          <Button basic inverted content="Register" style={{ marginLeft: "0.5em" }} />
+        </MenuItem>
+      </Container>
+    </Menu>
+  )
+}
