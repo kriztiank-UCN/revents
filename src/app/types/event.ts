4e939392e7fe4a0af8c57043a0405f1d738fc60d
Kristian <10221425@ucn.dk>
2024-04-28 15:16:53 +0200

    Using types with Typescript

---
+++ src/app/types/event.ts
@@ -0,0 +1,24 @@
+export type AppEvent = {
+  id: string
+  title: string
+  date: string
+  category: string
+  description: string
+  city: string
+  venue: string
+  // hostUid: string
+  hostedBy: string
+  hostPhotoURL: string
+  // isCancelled: boolean
+  attendees: Attendee[]
+  // attendeeIds: string[]
+  // isHost?: boolean
+  // isGoing?: boolean
+  // latLng?: LatLng
+}
+
+export type Attendee = {
+  id: string
+  displayName: string
+  photoURL: string
+}
