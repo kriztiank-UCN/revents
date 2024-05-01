1767fa4d428d09206388bdda4d8d9d87b410899b
Kristian <10221425@ucn.dk>
2024-05-01 09:19:37 +0200

    Redux actions test

--- src/features/scratch/Scratch.tsx
+++ src/features/scratch/Scratch.tsx
@@ -1,18 +1,20 @@
-// import { Button } from 'semantic-ui-react'
-import { useAppSelector } from "../../app/store/store"
-// import { decrement, increment, incrementByAmount } from './testSlice';
+import { Button } from 'semantic-ui-react'
+import { useAppDispatch, useAppSelector } from "../../app/store/store"
+import { decrement, increment, incrementByAmount } from './testSlice'
 
 export default function Scratch() {
+  // useAppSelector is a hook that allows us to access the state of the store
   const { data } = useAppSelector(state => state.test)
-  // const dispatch = useAppDispatch();
+  // useAppDispatch is a hook that allows us to dispatch actions to the store to update the data
+  const dispatch = useAppDispatch();
 
   return (
     <div>
       <h1>Scratch page</h1>
       <h3>The data is: {data}</h3>
-      {/* <Button onClick={() => dispatch(increment())} color='green' content='Increment' />
-            <Button onClick={() => dispatch(decrement())} color='red' content='Decrement' />
-            <Button onClick={() => dispatch(incrementByAmount(5))} color='teal' content='Increment by 5' /> */}
+        <Button onClick={() => dispatch(increment())} color='green' content='Increment' />
+        <Button onClick={() => dispatch(decrement())} color='red' content='Decrement' />
+        <Button onClick={() => dispatch(incrementByAmount(5))} color='teal' content='Increment by 5' />
     </div>
   )
 }
--- src/features/scratch/testSlice.ts
+++ src/features/scratch/testSlice.ts
@@ -12,16 +12,18 @@ export const testSlice = createSlice({
     name: 'test',
     initialState,
     reducers: {
-        // increment: (state) => {
-        //     state.data += 1
-        // },
-        // decrement: (state) => {
-        //     state.data -= 1
-        // },
-        // incrementByAmount: (state, action) => {
-        //     state.data += action.payload
-        // }
+      // Action creators are generated for each reducer function
+        increment: (state) => {
+            state.data += 1
+        },
+        decrement: (state) => {
+            state.data -= 1
+        },
+        // the action is an object that can take a payload of data which will be added to the state and updated
+        incrementByAmount: (state, action) => {
+            state.data += action.payload
+        }
     }
 })
-
-// export const {increment, decrement, incrementByAmount} = testSlice.actions
+// export the action creators
+export const {increment, decrement, incrementByAmount} = testSlice.actions
