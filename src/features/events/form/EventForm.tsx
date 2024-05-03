fc3da8a48ae1717eea575363deafb70af01c4492
Kristian <10221425@ucn.dk>
2024-05-03 09:56:36 +0200

    Setting up React hook form

--- src/features/events/form/EventForm.tsx
+++ src/features/events/form/EventForm.tsx
@@ -1,104 +1,62 @@
-import { ChangeEvent, useState } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import { Button, Form, Header, Segment } from 'semantic-ui-react';
-import { useAppDispatch, useAppSelector } from '../../../app/store/store';
-import { createEvent, updateEvent } from '../eventSlice';
-import { createId } from '@paralleldrive/cuid2';
+import { ChangeEvent, useState } from "react"
+import { Link, useNavigate, useParams } from "react-router-dom"
+import { Button, Form, Header, Segment } from "semantic-ui-react"
+import { useAppDispatch, useAppSelector } from "../../../app/store/store"
+import { createEvent, updateEvent } from "../eventSlice"
+import { createId } from "@paralleldrive/cuid2"
+import { FieldValues, useForm } from "react-hook-form"
 
 export default function EventForm() {
-  let {id} = useParams();
-  const event = useAppSelector(state => state.events.events.find(e => e.id === id));
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm()
+  let { id } = useParams()
+  const event = useAppSelector(state => state.events.events.find(e => e.id === id))
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
 
-  // if we have a selected event, we'll use that as the initial values
-  const initialValues = event ?? {
-    title: '',
-    category: '',
-    description: '',
-    city: '',
-    venue: '',
-    date: ''
-}
-
-  const [values, setValues] = useState(initialValues)
-
-  function onSubmit() {
-    id = id ?? createId();
-    event 
-        ? dispatch(updateEvent({...event, ...values}))
-        : dispatch(createEvent({...values, id, hostedBy: 'bob', attendees: [], hostPhotoURL: ''}))
-    navigate(`/events/${id}`);
-  }
-
-  // we'll use the name and value to update our state
-  function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
-    // get the name and value from the input fields
-    const { name, value } = e.target
-    // update the state with the new value
-    setValues({ ...values, [name]: value })
+  function onSubmit(data: FieldValues) {
+    console.log(data)
+    // id = id ?? createId()
+    // event
+    //   ? dispatch(updateEvent({ ...event, ...values }))
+    //   : dispatch(createEvent({ ...values, id, hostedBy: "bob", attendees: [], hostPhotoURL: "" }))
+    // navigate(`/events/${id}`)
   }
 
   return (
     <Segment clearing>
-     <Header content={event ? 'Update event' : 'Create Event'} />
-      <Form onSubmit={onSubmit}>
-        <Form.Field>
-          <input
-            type="text"
-            placeholder="Event title"
-            value={values.title}
-            name="title"
-            onChange={e => handleInputChange(e)}
-          />
-        </Form.Field>
-        <Form.Field>
-          <input
-            type="text"
-            placeholder="Category"
-            value={values.category}
-            name="category"
-            onChange={e => handleInputChange(e)}
-          />
-        </Form.Field>
-        <Form.Field>
-          <input
-            type="text"
-            placeholder="Description"
-            value={values.description}
-            name="description"
-            onChange={e => handleInputChange(e)}
-          />
-        </Form.Field>
-        <Form.Field>
-          <input
-            type="text"
-            placeholder="City"
-            value={values.city}
-            name="city"
-            onChange={e => handleInputChange(e)}
-          />
-        </Form.Field>
-        <Form.Field>
-          <input
-            type="text"
-            placeholder="Venue"
-            value={values.venue}
-            name="venue"
-            onChange={e => handleInputChange(e)}
-          />
-        </Form.Field>
-        <Form.Field>
-          <input
-            type="date"
-            placeholder="Date"
-            value={values.date}
-            name="date"
-            onChange={e => handleInputChange(e)}
-          />
-        </Form.Field>
+      <Header content={event ? "Update event" : "Create Event"} />
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Input
+          placeholder="Event title"
+          defaultValue={event?.title || ""}
+          {...register("title")}
+        />
+
+        <Form.Input
+          placeholder="Category"
+          defaultValue={event?.category || ""}
+          {...register("category")}
+        />
+
+        <Form.Input
+          placeholder="Description"
+          defaultValue={event?.description || ""}
+          {...register("description")}
+        />
+
+        <Form.Input placeholder="City" defaultValue={event?.city || ""} {...register("city")} />
+
+        <Form.Input placeholder="Venue" defaultValue={event?.venue || ""} {...register("venue")} />
+
+        <Form.Input
+          type="date"
+          placeholder="Date"
+          defaultValue={event?.date || ""}
+          {...register("date")}
+        />
+
         <Button type="submit" floated="right" positive content="Submit" />
-        <Button as={Link} to='/events' type='button' floated='right' content='Cancel' />
+        <Button as={Link} to="/events" type="button" floated="right" content="Cancel" />
       </Form>
     </Segment>
   )
