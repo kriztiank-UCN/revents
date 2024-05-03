ecd11dca9719627cc9a150a4912fe15ff61799b7
Kristian <10221425@ucn.dk>
2024-05-03 10:27:53 +0200

    Adding validation

--- src/features/events/form/EventForm.tsx
+++ src/features/events/form/EventForm.tsx
@@ -7,7 +7,11 @@ import { createId } from "@paralleldrive/cuid2"
 import { FieldValues, useForm } from "react-hook-form"
 
 export default function EventForm() {
-  const { register, handleSubmit } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid},
+  } = useForm({ mode: "onTouched" })
   let { id } = useParams()
   const event = useAppSelector(state => state.events.events.find(e => e.id === id))
   const dispatch = useAppDispatch()
@@ -29,33 +33,49 @@ export default function EventForm() {
         <Form.Input
           placeholder="Event title"
           defaultValue={event?.title || ""}
-          {...register("title")}
+          {...register("title", { required: true })}
+          error={errors.title && "Title is required"}
         />
 
         <Form.Input
           placeholder="Category"
           defaultValue={event?.category || ""}
-          {...register("category")}
+          {...register("category", { required: "Category is required" })}
+          error={errors.category && errors.category.message}
         />
 
         <Form.Input
           placeholder="Description"
           defaultValue={event?.description || ""}
-          {...register("description")}
+          {...register("description", { required: "Description is required" })}
+          error={errors.description && errors.description.message}
         />
 
-        <Form.Input placeholder="City" defaultValue={event?.city || ""} {...register("city")} />
+        <Form.Input
+          placeholder="City"
+          defaultValue={event?.city || ""}
+          {...register("city", { required: "City is required" })}
+          error={errors.city && errors.city.message}
+        />
 
-        <Form.Input placeholder="Venue" defaultValue={event?.venue || ""} {...register("venue")} />
+        <Form.Input
+          placeholder="Venue"
+          defaultValue={event?.venue || ""}
+          {...register("venue", { required: "Venue is required" })}
+          error={errors.venue && errors.venue.message}
+        />
 
         <Form.Input
           type="date"
           placeholder="Date"
           defaultValue={event?.date || ""}
-          {...register("date")}
+          {...register("date", {required: 'Date is required'})}
+          error={errors.date && errors.date.message}
         />
 
-        <Button type="submit" floated="right" positive content="Submit" />
+        <Button 
+        disabled={!isValid}
+        type="submit" floated="right" positive content="Submit" />
         <Button as={Link} to="/events" type="button" floated="right" content="Cancel" />
       </Form>
     </Segment>
