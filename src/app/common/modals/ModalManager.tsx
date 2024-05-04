9d6b05dbe4934589fa6c7c4cf100db9977de6505
Kristian <10221425@ucn.dk>
2024-05-04 09:31:37 +0200

    Adding a login form

--- src/app/common/modals/ModalManager.tsx
+++ src/app/common/modals/ModalManager.tsx
@@ -1,24 +1,21 @@
-// import LoginForm from '../../../features/auth/LoginForm';
-import TestModal from '../../../features/scratch/TestModal'
-import { useAppSelector } from '../../store/store'
+import LoginForm from "../../../features/auth/LoginForm"
+import TestModal from "../../../features/scratch/TestModal"
+import { useAppSelector } from "../../store/store"
 
 export default function ModalManager() {
-    const modalLookup = {
-        TestModal,
-        // LoginForm
-    }
+  const modalLookup = {
+    TestModal,
+    LoginForm,
+  }
 
-    const {type, data, open} = useAppSelector(state => state.modals);
+  const { type, data, open } = useAppSelector(state => state.modals)
 
-    let renderedModal;
+  let renderedModal
 
-    if (open && type) {
-        const ModalComponent = (modalLookup as any)[type];
-        renderedModal = <ModalComponent data={data} />
-    }
+  if (open && type) {
+    const ModalComponent = (modalLookup as any)[type]
+    renderedModal = <ModalComponent data={data} />
+  }
 
-
-    return (
-        <span>{renderedModal}</span>
-    )
+  return <span>{renderedModal}</span>
 }
--- src/app/layout/nav/SignedOutButtons.tsx
+++ src/app/layout/nav/SignedOutButtons.tsx
@@ -1,21 +1,16 @@
 import { MenuItem, Button } from 'semantic-ui-react';
-// import { useAppDispatch } from '../../store/store';
-// import { openModal } from '../../common/modals/modalSlice';
+import { useAppDispatch } from '../../store/store';
+import { openModal } from '../../common/modals/modalSlice';
 
-type Props = {
-    setAuth: (value: boolean) => void;
-}
-
-export default function SignedOutButtons({setAuth}: Props) {
-    // const dispatch = useAppDispatch();
+export default function SignedOutButtons() {
+    const dispatch = useAppDispatch();
 
     return (
         <MenuItem position='right'>
             <Button 
                 basic inverted 
                 content='Login' 
-                // onClick={() => dispatch(openModal({type: 'LoginForm'}))} />
-                onClick={() => setAuth(true)} />
+                onClick={() => dispatch(openModal({type: 'LoginForm'}))} />
             <Button 
                 basic 
                 inverted content='Register' 
