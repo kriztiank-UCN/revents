afa1ced0c2110049bdff453e07ebf5cdc94bc390
Kristian <10221425@ucn.dk>
2024-05-11 07:16:57 +0200

    Custom validation for the inputs

--- src/app/layout/nav/SignedInMenu.tsx
+++ src/app/layout/nav/SignedInMenu.tsx
@@ -20,6 +20,7 @@ export default function SignedInMenu() {
         <Dropdown.Menu>
           <Dropdown.Item as={Link} to="/createEvent" text="Create event" icon="plus" />
           <Dropdown.Item text="My profile" icon="user" />
+          <Dropdown.Item as={Link} to='/account' text='My account' icon='settings' />
           <Dropdown.Item onClick={handleSignOut} text="Sign out" icon="power" />
         </Dropdown.Menu>
       </Dropdown>
--- src/app/router/Routes.tsx
+++ src/app/router/Routes.tsx
@@ -4,8 +4,7 @@ import EventDashboard from "../../features/events/dashboard/EventDashboard"
 import EventDetailedPage from "../../features/events/details/EventDetailedPage"
 import EventForm from "../../features/events/form/EventForm"
 import Scratch from "../../features/scratch/Scratch"
-// import Scratch from '../../features/scratch/Scratch';
-// import AccountPage from '../../features/auth/AccountPage';
+import AccountPage from "../../features/auth/AccountPage"
 // import ProfilePage from '../../features/profiles/ProfilePage';
 // import RequireAuth from './RequireAuth';
 // import UnauthComponent from '../layout/UnauthComponent';
@@ -20,7 +19,8 @@ export const router = createBrowserRouter([
       { path: "/events/:id", element: <EventDetailedPage /> },
       { path: "/manage/:id", element: <EventForm /> },
       // EventForm with a key makes it unique and will rerender the component
-      { path: "/createEvent", element: <EventForm key='create' /> },
+      { path: "/createEvent", element: <EventForm key="create" /> },
+      { path: "/account", element: <AccountPage /> },
       { path: "/scratch", element: <Scratch /> },
     ],
   },
