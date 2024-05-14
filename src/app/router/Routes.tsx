497092f1ba0368210f4982c0e1ee1620d9183027
Kristian <10221425@ucn.dk>
2024-05-14 08:44:06 +0200

    User profiles

--- src/app/router/Routes.tsx
+++ src/app/router/Routes.tsx
@@ -5,7 +5,7 @@ import EventDetailedPage from "../../features/events/details/EventDetailedPage"
 import EventForm from "../../features/events/form/EventForm"
 import Scratch from "../../features/scratch/Scratch"
 import AccountPage from "../../features/auth/AccountPage"
-// import ProfilePage from '../../features/profiles/ProfilePage';
+import ProfilePage from '../../features/profiles/ProfilePage';
 // import RequireAuth from './RequireAuth';
 // import UnauthComponent from '../layout/UnauthComponent';
 // import MapsWrapper from '../common/maps/MapsWrapper';
@@ -20,6 +20,7 @@ export const router = createBrowserRouter([
       { path: "/manage/:id", element: <EventForm /> },
       // EventForm with a key makes it unique and will rerender the component
       { path: "/createEvent", element: <EventForm key="create" /> },
+      {path: '/profiles/:id', element: <ProfilePage />},
       { path: "/account", element: <AccountPage /> },
       { path: "/scratch", element: <Scratch /> },
     ],
--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,25 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { testSlice } from '../../features/scratch/testSlice';
-import { eventSlice } from '../../features/events/eventSlice';
-import { modalSlice } from '../common/modals/modalSlice';
-import { authSlice } from '../../features/auth/authSlice';
-// import { profileSlice } from '../../features/profiles/profileSlice';
-// import { photoSlice } from '../../features/profiles/photosSlice';
+import { configureStore } from "@reduxjs/toolkit"
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
+import { testSlice } from "../../features/scratch/testSlice"
+import { eventSlice } from "../../features/events/eventSlice"
+import { modalSlice } from "../common/modals/modalSlice"
+import { authSlice } from "../../features/auth/authSlice"
+import { profileSlice } from "../../features/profiles/profileSlice"
+import { photoSlice } from "../../features/profiles/photosSlice"
 // import { followSlice } from '../../features/profiles/follow/followSlice';
 // import { feedSlice } from '../../features/events/dashboard/feedSlice';
 
 export const store = configureStore({
-    reducer: {
-        test: testSlice.reducer,
-        events: eventSlice.reducer,
-        modals: modalSlice.reducer,
-        auth: authSlice.reducer,
-        // profiles: profileSlice.reducer,
-        // photos: photoSlice.reducer,
-        // follows: followSlice.reducer,
-        // feed: feedSlice.reducer
-    }
+  reducer: {
+    test: testSlice.reducer,
+    events: eventSlice.reducer,
+    modals: modalSlice.reducer,
+    auth: authSlice.reducer,
+    profiles: profileSlice.reducer,
+    photos: photoSlice.reducer,
+    // follows: followSlice.reducer,
+    // feed: feedSlice.reducer
+  },
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
